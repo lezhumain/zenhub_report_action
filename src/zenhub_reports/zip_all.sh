8bc44e62fd1dc401dc2f0e44d192f3f3e98a2b87
lezhumain <julien.merion@whitespace.co.uk>
2024-05-27 01:57:23 +0100

    shellcheck bash scripts

--- src/zenhub_reports/zip_all.sh
+++ src/zenhub_reports/zip_all.sh
@@ -18,8 +18,9 @@ fi
 rm -f ../zenhub_reports.zip
 mv dist /tmp/dist
 
-which 7z
-if [ "$?" == "0" ]; then
+#which 7z
+#if [ "$?" == "0" ]; then
+if which 7z; then
   7z a -tzip ../zenhub_reports.zip ../zenhub_reports
 else
   zip -r ../zenhub_reports.zip ../zenhub_reports
