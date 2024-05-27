336a9ed243dab2a8ebab7392dc78deb4763d86a3
lezhumain <julien.merion@whitespace.co.uk>
2024-05-27 02:33:21 +0100

    fix some eslint stuff

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
 import * as core from '@actions/core'
 import { IMainConfig, Program } from './zenhub_reports/src/zenhub_call'
-import { IIssue } from './zenhub_reports/src/models'
+import { IGhEvent, IIssue } from './zenhub_reports/src/models'
 import * as fs from 'node:fs'
 
-const current = new Date();
-const minus1month = new Date(current);
-minus1month.setMonth(minus1month.getMonth() - 1);
+const current = new Date()
+const minus1month = new Date(current)
+minus1month.setMonth(minus1month.getMonth() - 1)
 
-if(!process.env.WORKSPACE_ID || !process.env.REPO_ID) {
-    console.error("Need to export WORKSPACE_ID and REPO_ID");
-    process.exit(1);
+if (!process.env.WORKSPACE_ID || !process.env.REPO_ID) {
+  console.error('Need to export WORKSPACE_ID and REPO_ID')
+  process.exit(1)
 }
 
 const config: IMainConfig = {
-    workspaceId: process.env.WORKSPACE_ID || '5e3018c2d1715f5725d0b8c7',
-    outputJsonFilename: 'output/allEvs.json',
-    outputImageFilename: `output/output_average.png`,
-    minDate: minus1month.toISOString(),
-    maxDate: current.toISOString(),
-    labels: [],
-    skipRepos: [],
-    includeRepos: process.env.REPO_ID ? [Number(process.env.REPO_ID)] : [],
-    issuesToSkip: [],
-    fromPipeline: 'Backlog',
-    toPipeline: 'Awaiting TESS Review',
-    maxCount: 5,
-    release: ''
+  workspaceId: process.env.WORKSPACE_ID || '5e3018c2d1715f5725d0b8c7',
+  outputJsonFilename: 'output/allEvs.json',
+  outputImageFilename: `output/output_average.png`,
+  minDate: minus1month.toISOString(),
+  maxDate: current.toISOString(),
+  labels: [],
+  skipRepos: [],
+  includeRepos: process.env.REPO_ID ? [Number(process.env.REPO_ID)] : [],
+  issuesToSkip: [],
+  fromPipeline: 'Backlog',
+  toPipeline: 'Awaiting TESS Review',
+  maxCount: 5,
+  release: ''
 }
 
 /**
@@ -35,54 +35,50 @@ const config: IMainConfig = {
 export async function run(): Promise<void> {
   try {
     const program = new Program(config)
-    // skip ReBrowse
-    program
-      .main(
-        (issue: IIssue) => {
-          const matchesLabel: boolean =
-            config.labels !== undefined &&
-            config.labels.some(l => {
-              const low = l.toLowerCase()
-              return (
-                issue.labels !== undefined &&
-                issue.labels.map(la => la.toLowerCase()).includes(low)
-              )
-            })
-          const idShouldSkip: boolean = !!config.issuesToSkip?.includes(
-            issue.number
-          )
 
-          const skip = !matchesLabel && idShouldSkip
-          return Promise.resolve(skip)
-        },
-        (event: any) => {
-          if (!config.minDate || !config.maxDate) {
-            return Promise.resolve(false)
-          }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const res: any = program.main(
+      async (issue: IIssue) => {
+        const matchesLabel: boolean =
+          config.labels !== undefined &&
+          config.labels.some(l => {
+            const low = l.toLowerCase()
+            return (
+              issue.labels !== undefined &&
+              issue.labels.map(la => la.toLowerCase()).includes(low)
+            )
+          })
+        const idShouldSkip = !!config.issuesToSkip?.includes(issue.number)
 
-          const minDate: Date | undefined = new Date(config.minDate)
-          const maxDate: Date | undefined = new Date(config.maxDate)
-
-          const eventDate: Date = new Date(event.createdAt)
-          const skip: boolean =
-            (minDate !== undefined &&
-              eventDate.getTime() < minDate.getTime()) ||
-            (maxDate !== undefined && eventDate.getTime() > maxDate.getTime())
-
-          return Promise.resolve(skip)
+        const skip = !matchesLabel && idShouldSkip
+        return Promise.resolve(skip)
+      },
+      async (event: IGhEvent) => {
+        if (!config.minDate || !config.maxDate) {
+          return Promise.resolve(false)
         }
-      )
-      .then((res: any) => {
-        const file = 'zenhub_report.md';
-        fs.writeFileSync(file, res.mark, { encoding: 'utf8' })
-        core.setOutput('markdownContent', res.mark);
-        // core.setOutput('markdownFile', file);
-        // console.log('markdownContent', res.mark)
-      })
+
+        const minDate: Date | undefined = new Date(config.minDate)
+        const maxDate: Date | undefined = new Date(config.maxDate)
+
+        const eventDate: Date = new Date(event.createdAt)
+        const skip: boolean =
+          (minDate !== undefined && eventDate.getTime() < minDate.getTime()) ||
+          (maxDate !== undefined && eventDate.getTime() > maxDate.getTime())
+
+        return Promise.resolve(skip)
+      }
+    )
+
+    const file = 'zenhub_report.md'
+    fs.writeFileSync(file, res.mark, { encoding: 'utf8' })
+    core.setOutput('markdownContent', res.mark)
+    // core.setOutput('markdownFile', file);
+    // console.log('markdownContent', res.mark)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) {
-      core.setFailed(error.message);
+      core.setFailed(error.message)
       // console.log(error.message)
     }
   }
--- src/zenhub_reports/webpack.config.js
+++ src/zenhub_reports/webpack.config.js
@@ -1,28 +1,31 @@
-const path = require('path');
-const webpack = require('webpack');
+// eslint-disable-next-line @typescript-eslint/no-require-imports,import/no-commonjs,@typescript-eslint/no-var-requires
+const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-require-imports,import/no-commonjs,@typescript-eslint/no-var-requires
+const webpack = require('webpack')
 
+// eslint-disable-next-line import/no-commonjs
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
-            resource.request = resource.request.replace(/^node:/, "");
-        }),
-    ],
-    target: "node"
-};
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(/node:/, resource => {
+      resource.request = resource.request.replace(/^node:/, '')
+    })
+  ],
+  target: 'node'
+}
