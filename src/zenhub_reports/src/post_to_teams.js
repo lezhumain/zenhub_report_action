ca866f5ca3c379df22cf6790985e04f42df81b0c
lezhumain <julien.merion@whitespace.co.uk>
2024-05-28 05:46:36 +0100

    stuff

--- src/zenhub_reports/src/post_to_teams.js
+++ src/zenhub_reports/src/post_to_teams.js
@@ -21,8 +21,7 @@ const formattedCardPayload = {
   ]
 }
 
-const webhookUrl =
-  'https://verisk.webhook.office.com/webhookb2/a9380ca5-b775-432d-a35c-d7c0b976d157@3b07dc1f-22e7-4be1-ac66-a88bf3550222/IncomingWebhook/0af76c188e9947588eae47679a781340/9f3a21e6-f066-427f-af5e-1a5ce12e0b27'
+const webhookUrl = process.env.TEAMS_WEBHOOK
 
 axios
   .post(webhookUrl, formattedCardPayload)
