b2d8ffa2c1f1ea8a03669a54c8b0019f88232177
lezhumain <julien.merion@whitespace.co.uk>
2024-06-21 15:09:07 +0100

    new version

---
+++ run_with_inputs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+export INPUT_WORKSPACE_ID="582ffb92abc60d5d34359ef4"
+#export INPUT_REPO_ID="93615076"
+#export INPUT_REPO_ID="93615076,329683287"
+export INPUT_REPO_ID=""
+export INPUT_FROM_DATE="2024-06-06"
+export INPUT_TO_DATE="2024-06-13"
+export INPUT_FROM_PIPELINE="New Issues"
+export INPUT_TO_PIPELINE="Awaiting STAGING Release"
+export INPUT_RELEASE="2.19"
+export LABEL=""
+
+node dist/index.js
